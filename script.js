63c2b86c7bacdd2755f79246d927da0596d2fb23
nisatnilofficial0271 <107569069+nisatnilofficial0271@users.noreply.github.com>
2023-03-29 14:01:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,22 @@ window.addEventListener("load", function() {
 
 });
 
+// smooth scrolling to top on click of back to top button
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // show back to top button when user scrolls down 300px
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else {
+    if(backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none
